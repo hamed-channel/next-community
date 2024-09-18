5e24a966c3d1014106a46dd0c2ed0b1f62cd4bd9
Hamed Bahram <64147793+HamedBahram@users.noreply.github.com>
2024-09-18 09:28:10 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container'>
+        <h1 className='text-2xl font-semibold'>Next Shadcn Template</h1>
+      </div>
+    </section>
+  )
+}
